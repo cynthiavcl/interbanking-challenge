f86f4b978efa00d5a91e0e472caec4de72f338c8
Cynthia Vocal <vocal.cynthia@gmail.com>
2024-06-19 22:09:54 -0300

    project creation

---
+++ interbanking/src/test/java/com/api/interbanking/InterbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.interbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
